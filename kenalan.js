7a0a8fb16daf5e15e632e29f790449110667404e
rubianto28011 <32722751+rubianto28011@users.noreply.github.com>
2017-10-12 09:21:16 +0800

    file pertamaku
    inilah file javascript pertamaku

---
+++ kenalan.js
@@ -0,0 +1,7 @@
+var nama;
+ nama=prompt('Boleh tahu siapa nama anda?');
+ if(nama){
+  alert('Apa kabar '+nama+'! Saya Javascript, senang berkenalan dengan Anda');
+ }else{
+  alert('Hhmm sepertinya anda tidak mau berkenalan dengan saya.');
+}
